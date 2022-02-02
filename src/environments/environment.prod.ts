5fb9cda06d4bcfa32bee1ebfa271aef5cea05d1d
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-02-02 15:55:29 -0600

    add enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  api:{
+    login:{
+      url:'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword',
+      key:'AIzaSyB-p8CKoaQr097NJ8YJRpoWpezJj5xRRUI'
+    }
+  }
 };
