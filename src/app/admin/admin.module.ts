ea91249c23264e413f256d1597f940a6c3a52860
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-02-02 14:46:28 -0600

    :package: add component admin

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from '../shared/material/material.module';
+
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MaterialModule
+  ]
+})
+export class AdminModule { }
