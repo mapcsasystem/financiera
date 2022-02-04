2e86f63a6ef540f2e519c6582d5327d54fa48038
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-02-04 16:58:31 -0600

    add functions

---
+++ functions/server.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = {
+    db,
+    auth
+}
