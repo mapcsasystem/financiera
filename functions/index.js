f191b65d7a18905d0a7695daa701046a291ce1f0
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-02-07 13:50:45 -0600

    docs

--- functions/index.js
+++
@@ -1,3 +0,0 @@
-const functions = require("firebase-functions");
-const customers = require('./src/customers/customers');
-exports.customers = functions.https.onRequest(customers);
--- functions/src/customers/customers.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const createCustomersController = require('./controllers/create-customers.controller');
-const getCustomersController = require('./controllers/get-customers.controller');
-
-const router = Router();
-router.get('/', getCustomersController);
-router.post('/', createCustomersController);
-
-module.exports = router;
